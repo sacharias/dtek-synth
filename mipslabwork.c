8dd260f442876ff774bc8a2ed83371c9c77f68ae
Sacharias Sjöqvist <sacharias.sjoqvist@gmail.com>
2017-12-05 20:35:55 +0100

    fixed matrix support

--- mipslabwork.c
+++ mipslabwork.c
@@ -3,9 +3,8 @@
 #include "mipslab.h" /* Declatations for these labs */
 #include "generateSound.h"
 
-
 //the opcodes for the MAX7221 and MAX7219
-#define OP_NOOP   0
+#define OP_NOOP 0
 #define OP_DIGIT0 1
 #define OP_DIGIT1 2
 #define OP_DIGIT2 3
@@ -14,10 +13,10 @@
 #define OP_DIGIT5 6
 #define OP_DIGIT6 7
 #define OP_DIGIT7 8
-#define OP_DECODEMODE  9
-#define OP_INTENSITY   10
-#define OP_SCANLIMIT   11
-#define OP_SHUTDOWN    12
+#define OP_DECODEMODE 9
+#define OP_INTENSITY 10
+#define OP_SCANLIMIT 11
+#define OP_SHUTDOWN 12
 #define OP_DISPLAYTEST 15
 
 double count = 0.0;
@@ -25,46 +24,62 @@ int debug_value = 0;
 int sample = 0;
 int sampleMax = 411090;
 
+int globalCounter = 0;
+int currentState = 0;
+
 //int duration = 41109;
 
-
-void setCs(int high){
-  if(high){
+void setCs(int high)
+{
+  if (high)
+  {
     // Sätt cs till HIGH
     PORTESET = 0x0004;
-  } else {
+  }
+  else
+  {
     // Sätt cs till LOW
     PORTECLR = 0x0004;
   }
 }
 
-void setData(int high){
-  if(high){
+void setData(int high)
+{
+  if (high)
+  {
     // Sätt data till HIGH
     PORTESET = 0x0002;
-  } else {
+  }
+  else
+  {
     // Sätt DATA till LOW
     PORTECLR = 0x0002;
   }
 }
 
-void setClk(int high){
-  if(high){
+void setClk(int high)
+{
+  if (high)
+  {
     // Sätt clk till HIGH
     PORTESET = 0x0001;
-  } else {
+  }
+  else
+  {
     // Sätt clk till LOW
     PORTECLR = 0x0001;
   }
 }
 
 // Connect to 34 on chipkit
-void setupPlayButtons() {
+void setupPlayButtons()
+{
   TRISDSET = 0xFFFF; // Sätter första till input
-  TRISFSET = 0x8; // prova med 8
+  TRISFSET = 0x8;    // prova med 8
 }
 
-int* getPlayButtons() {
+int *getPlayButtons()
+{
   // Pin 5
   int playButton1 = PORTD >> 1;
   playButton1 &= 0x1;
@@ -95,9 +110,8 @@ int* getPlayButtons() {
   // playButton7 &= 0x1;
   int playButton7 = 0;
 
-
   int playButtonsValue = 0;
-  playButtonsValue = playButton1 << 6 ;
+  playButtonsValue = playButton1 << 6;
   playButtonsValue |= playButton2 << 5;
   playButtonsValue |= playButton3 << 4;
   playButtonsValue |= playButton4 << 3;
@@ -107,7 +121,6 @@ int* getPlayButtons() {
 
   return playButtonsValue;
 }
-
 
 /* Interrupt Service Routine */
 void user_isr()
@@ -121,28 +134,33 @@ void user_isr()
   //     value += getCValue(sample);
   //     n++;
   // }
-  if (playButtonsValue & 0x20) {
+  if (playButtonsValue & 0x20)
+  {
     PORTESET = 0x2;
     value += getDValue(sample);
     n++;
   }
 
-  if (playButtonsValue & 0x10) {
+  if (playButtonsValue & 0x10)
+  {
     value += getEValue(sample);
     n++;
   }
 
-  if (playButtonsValue & 0x8) {
+  if (playButtonsValue & 0x8)
+  {
     value += getFValue(sample);
     n++;
   }
 
-  if (playButtonsValue & 0x4) {
+  if (playButtonsValue & 0x4)
+  {
     value += getGValue(sample);
     n++;
   }
 
-  if (playButtonsValue & 0x2) {
+  if (playButtonsValue & 0x2)
+  {
     value += getAValue(sample);
     n++;
   }
@@ -153,168 +171,216 @@ void user_isr()
   //   n++;
   // }
 
-  if (n == 0) {
+  if (n == 0)
+  {
     OC1RS = 0;
-  } else {
-    OC1RS = ( value / n ) + 127;
+  }
+  else
+  {
+    OC1RS = (value / n) + 127;
   }
 
   sample++;
-  if (sample >= sampleMax) { sample = 0; }
+  if (sample >= sampleMax)
+  {
+    sample = 0;
+  }
 
   //clear interrupt flag
   IFS(0) = 0x0000;
 }
 
 void spiTransfer(char data[]) {
-    //Create an array with the data to shift out
-    // [0, 0, 0, 0, 1, 1, 1, 1]
-    setCs(0);
+  //Create an array with the data to shift out
+  // [0, 0, 0, 0, 1, 1, 1, 1]
+  setCs(0);
 
-    int i;
-    for (i = 0; i < 16; i++){
-      setClk(0);
-      setData(data[i]);
-      quicksleep(80);
-      setClk(1);
-      quicksleep(80);
-    }
-
-    setCs(1);
+  int i;
+  for (i = 0; i < 16; i++) {
     setClk(0);
+    setData(data[i]);
     quicksleep(80);
+    setClk(1);
+    quicksleep(80);
+  }
 
-
+  setCs(1);
+  setClk(0);
+  quicksleep(80);
 }
 
-void shutdownLamps(int address, int down){
-  if(down){
+void spiTransferDuo(char data1[], char data2[]) {
+  setCs(0);
+
+  int i;
+  for (i = 0; i < 16; i++) {
+    setClk(0);
+    setData(data2[i]);
+    quicksleep(80);
+    setClk(1);
+    quicksleep(80);
+  }
+
+  i = 0;
+  for (i = 0; i < 16; i++) {
+    setClk(0);
+    setData(data1[i]);
+    quicksleep(80);
+    setClk(1);
+    quicksleep(80);
+  }
+
+  setCs(1);
+  setClk(0);
+  quicksleep(80);
+}
+
+void shutdownLamps(int address, int down)
+{
+  if (down)
+  {
     //spiTransfer(address, OP_SHUTDOWN,(char)0);
   }
 }
 
-void deployMatrice(char *matrice){
+void deployMatrice(char *matrice)
+{
   int x = 16;
   int y = 8;
 
   // first row second led
-  char digit0[] = {0,0,0,0, 0,0,0,1, 0,0,0,0 ,0,0,0,0 };
+  char digit0[] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
   int i = 0;
-  for(i =0; i < y; i++){
+  for (i = 0; i < y; i++)
+  {
     digit0[8 + i] = matrice[i + 8];
   }
   spiTransfer(digit0);
 
   // first row first led
-  for(i =0; i < y; i++){
+  for (i = 0; i < y; i++)
+  {
     digit0[8 + i] = matrice[i];
   }
   spiTransfer(digit0);
 
   // second row second led
-  char digit1[] = {0,0,0,0, 0,0,1,0, 0,0,0,0 ,0,0,0,0 };
-  for(i =0; i < y; i++){
+  char digit1[] = {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (i = 0; i < y; i++)
+  {
     digit1[8 + i] = matrice[16 + i + 8];
   }
   spiTransfer(digit1);
 
   // second row first led
-  for(i =0; i < y; i++){
+  for (i = 0; i < y; i++)
+  {
     digit1[8 + i] = matrice[16 + i];
   }
   spiTransfer(digit1);
 
   // third row second led
-  char digit2[] = {0,0,0,0, 0,0,1,1, 0,0,0,0 ,0,0,0,0 };
-  for(i =0; i < y; i++){
+  char digit2[] = {0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (i = 0; i < y; i++)
+  {
     digit2[8 + i] = matrice[32 + i + 8];
   }
   spiTransfer(digit2);
 
   // third row first led
-  for(i =0; i < y; i++){
+  for (i = 0; i < y; i++)
+  {
     digit2[8 + i] = matrice[32 + i];
   }
   spiTransfer(digit2);
 
   // fourth row second led
-  char digit3[] = {0,0,0,0, 0,1,0,0, 0,0,0,0 ,1,0,0,0 };
-  for(i =0; i < y; i++){
+  char digit3[] = {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0};
+  for (i = 0; i < y; i++)
+  {
     digit3[8 + i] = matrice[48 + i + 8];
   }
   spiTransfer(digit3);
 
   // fourth row first led
-  for(i =0; i < y; i++){
+  for (i = 0; i < y; i++)
+  {
     digit3[8 + i] = matrice[48 + i];
   }
   spiTransfer(digit3);
 
   // fifth row second led
-  char digit4[] = {0,0,0,0, 0,1,0,1, 0,0,0,0 ,0,0,0,0 };
-  for(i =0; i < y; i++){
+  char digit4[] = {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (i = 0; i < y; i++)
+  {
     digit4[8 + i] = matrice[64 + i + 8];
   }
   spiTransfer(digit4);
 
   // fifth row first led
-  for(i =0; i < y; i++){
+  for (i = 0; i < y; i++)
+  {
     digit4[8 + i] = matrice[64 + i];
   }
   spiTransfer(digit4);
 
   // 6 row second led
-  char digit5[] = {0,0,0,0, 0,1,1,0, 0,0,1,0 ,0,0,0,0 };
-  for(i =0; i < y; i++){
+  char digit5[] = {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0};
+  for (i = 0; i < y; i++)
+  {
     digit5[8 + i] = matrice[80 + i + 8];
   }
   spiTransfer(digit5);
 
   // 6 row first led
-  for(i =0; i < y; i++){
+  for (i = 0; i < y; i++)
+  {
     digit5[8 + i] = matrice[80 + i];
   }
   spiTransfer(digit5);
 
   // 7 row second led
-  char digit6[] = {0,0,0,0, 0,1,1,1, 0,0,0,0 ,0,0,0,0 };
-  for(i =0; i < y; i++){
+  char digit6[] = {0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (i = 0; i < y; i++)
+  {
     digit6[8 + i] = matrice[96 + i + 8];
   }
   spiTransfer(digit6);
 
   // 7 row first led
-  for(i =0; i < y; i++){
+  for (i = 0; i < y; i++)
+  {
     digit6[8 + i] = matrice[96 + i];
   }
   spiTransfer(digit6);
 
   // 8 row second led
-  char digit7[] = {0,0,0,0, 1,0,0,0, 1,0,0,0 ,0,0,0,0 };
-  for(i =0; i < y; i++){
+  char digit7[] = {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
+  for (i = 0; i < y; i++)
+  {
     digit7[8 + i] = matrice[112 + i + 8];
   }
   spiTransfer(digit7);
 
   // 7 row first led
-  for(i =0; i < y; i++){
+  for (i = 0; i < y; i++)
+  {
     digit7[8 + i] = matrice[112 + i];
   }
   spiTransfer(digit7);
-
 }
 
-
-void test(){
+void test()
+{
   char twoDim[128];
 
   int y = 128;
 
   int i = 0;
-  for(i = 0; i < y; y++){
+  for (i = 0; i < y; y++)
+  {
     twoDim[i] = 0;
   }
-
   deployMatrice(twoDim);
 
   // 0000 0000 0000 0000
@@ -325,59 +391,138 @@ void test(){
   // 0000 0000 0000 0000
   // 0000 0000 0000 0000
   // 0000 0000 0000 0000
-
 }
 
-
-void initLamps(){
+void initLamps()
+{
   // Sätt upp output på ledlamporna
   TRISECLR = 0x0001;
   TRISECLR = 0x0002;
   TRISECLR = 0x0004;
   //int dataarray = {0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1};
-//  spiTransfer(dataarray);
-  char normalOperation[] = {0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1};
+  //  spiTransfer(dataarray);
+  char normalOperation[] = {0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
   spiTransfer(normalOperation);
 
-  char scanlimit[] = {0,0,0,0, 1,0,1,1, 0,0,0,0 ,0,1,1,1};
+  char scanlimit[] = {0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1};
   spiTransfer(scanlimit);
 
-  char intensity[] = {0,0,0,0, 1,0,1,0, 0,0,0,0 ,0,0,0,1};
+  char intensity[] = {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1};
   spiTransfer(intensity);
 
-  test();
-  // char digit0[] = {0,0,0,0, 0,0,0,1, 0,0,0,0 ,0,0,0,1 };
+  // test();
+
+  // VERSION 1 - DIAGONAL
+  // char digit0[] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1};
   // spiTransfer(digit0);
-  //
-  // char digit1[] = {0,0,0,0, 0,0,1,0, 0,0,0,0 ,0,0,1,0 };
+
+  // char digit1[] = {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0};
   // spiTransfer(digit1);
-  //
-  // char digit2[] = {0,0,0,0, 0,0,1,1, 0,0,0,0 ,0,1,0,0 };
+
+  // char digit2[] = {0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0};
   // spiTransfer(digit2);
-  //
-  // char digit3[] = {0,0,0,0, 0,1,0,0, 0,0,0,0 ,1,0,0,0 };
+
+  // char digit3[] = {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0};
   // spiTransfer(digit3);
-  //
-  // char digit4[] = {0,0,0,0, 0,1,0,1, 0,0,0,1 ,0,0,0,0 };
+
+  // char digit4[] = {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0};
   // spiTransfer(digit4);
-  //
-  // char digit5[] = {0,0,0,0, 0,1,1,0, 0,0,1,0 ,0,0,0,0 };
+
+  // char digit5[] = {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0};
   // spiTransfer(digit5);
-  //
-  // char digit6[] = {0,0,0,0, 0,1,1,1, 0,1,0,0 ,0,0,0,0 };
+
+  // char digit6[] = {0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0};
   // spiTransfer(digit6);
-  //
-  // char digit7[] = {0,0,0,0, 1,0,0,0, 1,0,0,0 ,0,0,0,0 };
+
+  // char digit7[] = {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
   // spiTransfer(digit7);
   // spiTransfer(digit7);
+
+  // RESET - turn off all lights
+  char digit10[] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+  spiTransfer(digit10);
+
+  char digit11[] = {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  spiTransfer(digit11);
+
+  char digit12[] = {0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+  spiTransfer(digit12);
+
+  char digit13[] = {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  spiTransfer(digit13);
+
+  char digit14[] = {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+  spiTransfer(digit14);
+
+  char digit15[] = {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  spiTransfer(digit15);
+
+  char digit16[] = {0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+  spiTransfer(digit16);
+
+  char digit17[] = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  spiTransfer(digit17);
+  spiTransfer(digit17);
 }
 
-
-
-
-
-
 // volatile int *triseAdress = 0xbf886100; //
+
+void setLedMatrix(char matrix[])
+{ 
+  char OneDigit0[] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0};
+  char TwoDigit0[] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1};
+  spiTransferDuo(OneDigit0, TwoDigit0);
+  
+
+  // char digit1[] = {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0};
+  // spiTransfer(digit1);
+
+  // char digit2[] = {0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0};
+  // spiTransfer(digit2);
+
+  // char digit3[] = {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0};
+  // spiTransfer(digit3);
+
+  // char digit4[] = {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0};
+  // spiTransfer(digit4);
+
+  char digit5[] = {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0};
+  // spiTransfer(digit5);
+
+  // char digit6[] = {0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0};
+  // spiTransfer(digit6);
+
+  // char digit7[] = {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
+  // spiTransfer(digit6);
+
+
+  // if (globalCounter >= 10000)
+  // {
+  //   if (currentState == 0)
+  //   {
+  //     char digit17[] = {0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1};
+  //     spiTransfer(digit17);
+  //     currentState++;
+  //   }
+  //   else if (currentState == 1)
+  //   {
+  //     char digit17[] = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1};
+  //     spiTransfer(digit17);
+  //     currentState++;
+  //   }
+  //   else
+  //   {
+  //     char digit17[] = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+  //     spiTransfer(digit17);
+  //     currentState = 0;
+  //   }
+  //   globalCounter = 0;
+  // }
+  // else
+  // {
+  //   globalCounter++;
+  // }
+}
 
 /* Lab-specific initialization goes here */
 void labinit()
@@ -385,7 +530,6 @@ void labinit()
   // *triseAdress &= ~0xFF;
   // TRISD |= 0xFE0; // toggle switches
 
-  initLamps();
   // set priority
   IPC(2) = 0x1c;
 
@@ -415,18 +559,25 @@ void labinit()
   enable_interrupt();
   generateSinWaves();
   setupPlayButtons();
-}
 
+  initLamps();
+
+  char matrix[128] = { 
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                };
+
+  setLedMatrix(matrix);
+}
 
 /* This function is called repetitively from the main program */
 void labwork()
 {
-
-
-
-
-
   int portD = getPlayButtons();
-
-
 }
